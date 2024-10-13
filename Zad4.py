f5ffe47ea8b0c85556a5f45efefc9a2985180408
Cortado-sh <guds.krieger.yhwach@gmail.com>
2024-10-13 21:27:20 +0200

    Add files via upload

---
+++ Zad4.py
@@ -0,0 +1,3 @@
+Cena=39.99
+Rabat=0.2
+print (round(Cena - Cena * Rabat, 2 ))
---
+++ Zad5.py
@@ -0,0 +1,8 @@
+print("Dzień dobry podaj wymiar boku a: ")
+a = int(input())
+print("Dzień dobry podaj wymiar boku b: ")
+b = int(input())
+pole = a * b
+obwod = a * b * 2
+
+print("Twój prostokąt ma obwód o wartości:", obwod, "i pole o wartości", pole)
